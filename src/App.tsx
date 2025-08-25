40035ba2691836beb46c5eac71de2d7d49041bec
mitraws <m.ws@outlook.com>
2025-08-25 22:55:45 +0200

    feat: fetch dog breeds from API and store in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import CompareDogsPage from './pages/CompareDogsPage'
 import HomePage from './pages/HomePage'
@@ -6,6 +6,14 @@ import HomePage from './pages/HomePage'
 function App() {
   return (
     <Router>
+      <nav style={{ margin: 10 }}>
+        <Link to='/' style={{ padding: 5 }}>
+          Home
+        </Link>
+        <Link to='/compare-dogs' style={{ padding: 5 }}>
+          Compare Dogs
+        </Link>
+      </nav>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/compare-dogs' element={<CompareDogsPage />} />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,42 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+interface Dog {
+  id: string
+  name: string
+}
 
 const HomePage = () => {
+  const [dogs, setDogs] = useState<Dog[]>([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    axios
+      .get('https://api.thedogapi.com/v1/breeds')
+      .then((res) => {
+        setDogs(res.data)
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
   return (
-    <div style={{ padding: '2rem' }}>
+    <>
+      {' '}
       <h1>Dog Breeds</h1>
-      <Link to='/compare-dogs'>Go to Compare Dogs</Link>
-    </div>
+      {loading ? (
+        <p>Loading…</p>
+      ) : (
+        <ul>
+          {dogs.map((dog) => (
+            <li key={dog.id}>{dog.name}</li>
+          ))}
+        </ul>
+      )}
+      <div style={{ padding: '2rem' }}>
+        <Link to='/compare-dogs'>Go to Compare Dogs</Link>
+      </div>{' '}
+    </>
   )
 }
 
