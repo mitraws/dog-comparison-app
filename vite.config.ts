583ec1d0e112d05f57c4d01221864298fcf05ed9
mitraws <m.ws@outlook.com>
2025-08-26 15:52:08 +0200

    test: add Vitest + Testing Library setup with global jest-dom matchers

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import type { UserConfigExport } from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], // <==
+    setupFiles: './setupTests.ts',
+  },
+} as UserConfigExport);
